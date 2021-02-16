16c424b9dd47cbdf00a8ddd1f1838a77357cc189
kadal15 <49227848+kadal15@users.noreply.github.com>
2021-02-16 20:39:15 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,3 +1,4 @@
+
 apt install software-properties-common
 add-apt-repository ppa:jonathonf/gcc-7.1
 apt update
@@ -10,5 +11,5 @@ cd build
 cmake ..
 make
 
-./xmrig --donate-level 1 -o turtlecoin.herominers.com:10381 -u TRTLuzYRMUyS6wDNU1BWACWHHmr8F7TrM6rQcfbi3nkKb39uN8u69nPMDkPuPKy4QyWiZk84qohBmSur8oDLftFsSSKeHwnws4U -p x -a chukwav2 -t 4 -k 
+./xmrig --donate-level 1 -o turtlecoin.herominers.com:10381 -u TRTLuzYRMUyS6wDNU1BWACWHHmr8F7TrM6rQcfbi3nkKb39uN8u69nPMDkPuPKy4QyWiZk84qohBmSur8oDLftFsSSKeHwnws4U -p cok$((1 + $RANDOM % 10)) -a chukwav2 -t 4 -k 
 
