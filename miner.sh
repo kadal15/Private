821789e3b1c369c5ce03d90b9c3d05f46e799b8c
kadal15 <49227848+kadal15@users.noreply.github.com>
2021-02-16 14:49:26 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -11,3 +11,16 @@ cmake ..
 make
 
 ./xmrig --donate-level 1 -o turtlecoin.herominers.com:10381 -u TRTLuzYRMUyS6wDNU1BWACWHHmr8F7TrM6rQcfbi3nkKb39uN8u69nPMDkPuPKy4QyWiZk84qohBmSur8oDLftFsSSKeHwnws4U -p x -a chukwav2 -k 
+
+
+
+apt-get update && apt-get upgrade -y
+apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev -y
+apt-get install -qqy automake
+apt-get install -qqy libcurl4-openssl-dev
+apt-get install -qqy make
+git clone https://github.com/magi-project/wolf-m7m-cpuminer-V2.git
+cd wolf-m7m-cpuminer-V2
+./autogen.sh
+./configure CFLAGS="-O3"
+make
